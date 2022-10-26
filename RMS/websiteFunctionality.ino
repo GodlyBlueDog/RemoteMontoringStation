e94bc46c3dc652d4c46f835133b6dad4f16ea4cf
Oliver Drew <99225553+GodlyBlueDog@users.noreply.github.com>
2022-10-27 10:28:54 +1100

    Made the web portal look nicer

--- RMS/websiteFunctionality.ino
+++ RMS/websiteFunctionality.ino
@@ -1,5 +1,16 @@
 void routesConfiguration() {
-
+  
+server.onNotFound([](AsyncWebServerRequest * request) {
+    if (request->url().endsWith(F(".jpg"))) {
+      // Extract the filename that was attempted
+      int fnsstart = request->url().lastIndexOf('/');
+      String fn = request->url().substring(fnsstart);
+      // Load the image from SPIFFS and send to the browser.
+      request->send(SPIFFS, fn, "image/jpeg", true);
+    } else {
+      request->send(SPIFFS, "/404.html");
+    }
+  });
   // Example of a 'standard' route
   // No Authentication
   server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest * request) {
